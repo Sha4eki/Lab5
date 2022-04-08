a11dd2e32cff51f5f0a2960bb2656341f17a272b
Sha4eki <shagek@mail.ru>
2022-04-08 12:01:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ultrapoop/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.ultrapoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private VideoView MyvideoPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyvideoPlayer = (VideoView)findViewById(R.id.videoView);
+        Uri myVideoUri= Uri.parse( "android.resource://" +
+        getPackageName() + "/" + R.raw.bonk);
+        MyvideoPlayer.setVideoURI(myVideoUri);
+        MediaController mediaController = new MediaController(this);
+        MyvideoPlayer.setMediaController(mediaController);
+        mediaController.setMediaPlayer(MyvideoPlayer);
+    }
+    public void onStartClick(View view)
+    {
+        MyvideoPlayer.start();
+    }
+    public void onPauseClick(View view)
+    {
+        MyvideoPlayer.pause();
+    }
+    public void onStopClick(View view)
+    {
+        MyvideoPlayer.stopPlayback();
+        MyvideoPlayer.resume();
+    }
+}
